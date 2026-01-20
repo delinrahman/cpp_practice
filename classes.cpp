10798cc487ee7c631dbee7b94cd51c6d409ef559
delinrahman <rdelin2004@gmail.com>
2026-01-20 19:22:35 +0600

    delin's 2nd commit

--- classes.cpp
+++ classes.cpp
@@ -1,52 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Log
-{
+class Log {
 public:
   const int Loglevelwarning = 0;
   const int LoglevelError = 1;
   const int LoglevelInfo = 2;
+
 private:
   int m_level;
+
 public:
-  void setLevel(int level)
-  {
-    m_level = level;
-  }
-  void warn(const char* message)
-  {
-    if(m_level >= Loglevelwarning)
-    {
-      cout << "[Warning]"<< message<< endl;
+  void setLevel(int level) { m_level = level; }
+  void warn(const char *message) {
+    if (m_level >= Loglevelwarning) {
+      cout << "[Warning]" << message << endl;
     }
   }
 
-
-  void Erro(const char* message)
-  {
-    if(m_level >= LoglevelError)
-    {
-      cout << "[error]"<< message<< endl;
+  void Erro(const char *message) {
+    if (m_level >= LoglevelError) {
+      cout << "[error]" << message << endl;
     }
   }
-  void info(const char* message)
-  {
-    if(m_level>=LoglevelInfo){
+  void info(const char *message) {
+    if (m_level >= LoglevelInfo) {
 
-      cout << "[Info]"<< message<< endl;
+      cout << "[Info]" << message << endl;
     }
   }
 };
 
-int main(){
+int main() {
   Log log;
   log.setLevel(2);
   log.warn("hello world");
   log.Erro("delin rahman");
-  log.info("mumdudur rahman"); 
-
-
-
+  log.info("mumdudur rahman");
+  /*delin rahmanh*/
 
   return 0;
 }
